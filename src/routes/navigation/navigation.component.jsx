0fec25d98e75523992e4c8c46ad869bc2e71bec4
tzhoji <57669967+tzhoji@users.noreply.github.com>
2023-04-07 00:35:32 +0800

    Add files via upload

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,48 @@
+import { Outlet,Link} from 'react-router-dom';
+import { Fragment,useContext} from 'react';
+import hkstlogo from '../../assets/logo5.jpg';
+import './navigation.styles.scss'
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+
+const Navigation = () => {
+  const {currentUser}=useContext(UserContext);
+  const {isCartOpen}=useContext(CartContext)
+
+  // console.log(currentUser)
+  return (
+    <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <img src={hkstlogo} alt="logo" />
+                  
+            </Link>
+        
+        <div className='nav-links-container'>
+            <Link className='nav-link' to="/shop">
+                SHOP
+          </Link>
+            {
+              currentUser?(
+                <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>)
+                : (
+                <Link className='nav-link' to="/auth">
+                  SIGN IN
+                </Link>
+                  )}
+                  <CartIcon/>
+                  
+
+        </div>
+                {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
